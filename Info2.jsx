7ed1c66ac0dc9199cd6d92ffbdda629cc8ba1088
DHANAVISHNU <83230941+DHANAVISHNU@users.noreply.github.com>
2021-06-26 18:08:45 +0530

    Add files via upload

---
+++ Info2.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Info2(){
+  return (
+    <div className="note">
+        <h1>Cyber security</h1>
+        <p>
+          A basic cyber security Bootcamp
+        </p>
+  </div>
+  );
+}
+
+export default Info2;
---
+++ Info3.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Info3(){
+  return (
+    <div className="note">
+        <h1>Data science</h1>
+        <p>
+          A basic Data science Bootcamp
+        </p>
+  </div>
+  );
+}
+
+export default Info3;
